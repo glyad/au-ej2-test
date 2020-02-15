4c280bf397b312d90054393cb70ce773cc0fd968
David Kossoglyad <david@logoui.co.il>
2020-02-15 22:19:33 +0200

    chore: initial commit

---
+++ src/dist/main.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+var environment = require("../config/environment.json");
+var aurelia_pal_1 = require("aurelia-pal");
+function configure(aurelia) {
+    aurelia.use
+        .standardConfiguration()
+        .feature(aurelia_pal_1.PLATFORM.moduleName('resources/index'));
+    aurelia.use.plugin(aurelia_pal_1.PLATFORM.moduleName('au-ej2-plugin'));
+    aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
+    if (environment.testing) {
+        aurelia.use.plugin(aurelia_pal_1.PLATFORM.moduleName('aurelia-testing'));
+    }
+    aurelia.start().then(function () { return aurelia.setRoot(aurelia_pal_1.PLATFORM.moduleName('app')); });
+}
+exports.configure = configure;
